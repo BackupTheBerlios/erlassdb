47a403b12ea56d09895cd95b7087fa83095cf746
maikel <maikel@users.berlios.de>
2011-11-14 18:24:45 +0100

    ereg -> preg_match

--- classes/Mailer.php
+++ classes/Mailer.php
@@ -28,11 +28,26 @@ class Mailer {
 
     /**
      * Checks for valid chars, but not for an address defined in RFC 2822.
+     * 
+     * <code>
+     * $result = Mailer::isValidAddress('maikel@localhost');
+     * var_dump($result); /// bool(true)
+     * </code>
+     * 
+     * <code>
+     * $result = Mailer::isValidAddress("maikel\t@localhost");
+     * var_dump($result); /// bool(false)
+     * </code>
+     *
+     * <code>
+     * $result = Mailer::isValidAddress('"maikel "@localhost');
+     * var_dump($result); /// bool(true)
+     * </code>
      * @param $mailAddress address to check
      * @return boolean seems to be valid or not
      */
     public static function isValidAddress($mailAddress) {
-        return (boolean) ereg('^[[:print:]]+$', $mailAddress);
+        return (boolean) preg_match('/^[[:print:]]+$/', $mailAddress);
     }
 
 }
